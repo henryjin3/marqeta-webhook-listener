ae2eeb6053858d3af555e35b0d1e365312f44fc5
henryjin3 <henry@cadenzadevelopment.com>
2021-08-25 12:26:43 -0500

    Add marqeta listener

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from 'express';
+import fs from 'fs';
 
 const app: Application = express();
 
@@ -10,4 +11,30 @@ app.get('/world', (req: Request, res: Response) => {
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
+});
+
+app.use(express.json());
+
+const FILE = './db.json';
+
+app.post('/marqeta', function (req: Request, res: Response) {
+  console.log(`Post received`);
+  fs.writeFile(FILE, JSON.stringify(req.body), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    res.send('Request saved');
+    console.log('Request saved');
+  });
+});
+
+app.get('/marqeta', function (req: Request, res: Response) {
+  fs.readFile(FILE, function (err, data) {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    res.send(data);
+  });
 });
